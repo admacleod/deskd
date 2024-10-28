89fe860cb4af6fbac281f0e6379eb57c5ce08956
Alisdair MacLeod <git@alisdairmacleod.co.uk>
2024-10-28 09:01:11 +0000

    Refactor database queries to be easier to reason about

--- internal/store/bookings.go
+++ internal/store/bookings.go
@@ -76,92 +76,58 @@ func (store *Bookings) Close() error {
 }
 
 const (
-	querySelectBookingsByDesk       = `SELECT id, user, desk, start, end FROM bookings WHERE desk = ?`
-	queryInsertBooking              = `INSERT INTO bookings (user, desk, start, end) VALUES (?,?,?,?)`
-	querySelectBookingsByDate       = `SELECT id, user, desk, start, end FROM bookings WHERE start = ?`
-	querySelectFutureBookingsByUser = `SELECT id, user, desk, start, end FROM bookings WHERE user = ? AND start >= DATE()`
-	queryDeleteBookingByID          = `DELETE FROM bookings WHERE id = ?`
+	execInsertBooking         = `INSERT INTO bookings (user, desk, start, end) VALUES (?,?,?,?)`
+	execDeleteBooking         = `DELETE FROM bookings WHERE id = ?`
+	queryBookingsByDesk       = `SELECT id, user, desk, start, end FROM bookings WHERE desk = ?`
+	queryBookingsByDate       = `SELECT id, user, desk, start, end FROM bookings WHERE start = ?`
+	queryFutureBookingsByUser = `SELECT id, user, desk, start, end FROM bookings WHERE user = ? AND start >= DATE()`
 )
 
-func (store *Bookings) GetDeskBookings(ctx context.Context, desk string) (_ []booking.Booking, err error) {
-	rows, err := store.db.QueryContext(ctx, querySelectBookingsByDesk, desk)
-	switch {
-	case errors.Is(err, sql.ErrNoRows):
-	case err != nil:
-		return nil, fmt.Errorf("retrieve bookings for desk %q from database: %w", desk, err)
-	}
-	defer func() {
-		if cerr := rows.Close(); cerr != nil && err == nil {
-			err = cerr
-		}
-	}()
-	var ret []booking.Booking
-	for rows.Next() {
-		var b booking.Booking
-		if err := rows.Scan(&b.ID, &b.User, &b.Desk, &b.Slot.Start, &b.Slot.End); err != nil {
-			return nil, fmt.Errorf("scan booking for desk %q from database: %w", desk, err)
-		}
-		ret = append(ret, b)
-	}
-	return ret, nil
-}
-
 func (store *Bookings) AddBooking(ctx context.Context, b booking.Booking) error {
-	if _, err := store.db.ExecContext(ctx, queryInsertBooking, b.User, b.Desk, b.Slot.Start, b.Slot.End); err != nil {
+	if _, err := store.db.ExecContext(ctx, execInsertBooking, b.User, b.Desk, b.Slot.Start, b.Slot.End); err != nil {
 		return fmt.Errorf("insert booking into database: %w", err)
 	}
 	return nil
 }
 
-func (store *Bookings) GetAllBookingsForDate(ctx context.Context, date time.Time) ([]booking.Booking, error) {
-	rows, err := store.db.QueryContext(ctx, querySelectBookingsByDate, date)
-	switch {
-	case errors.Is(err, sql.ErrNoRows):
-	case err != nil:
-		return nil, fmt.Errorf("retrieve bookings for date %q from database: %w", date, err)
-	}
-	defer func() {
-		if cerr := rows.Close(); cerr != nil && err == nil {
-			err = cerr
-		}
-	}()
-	var ret []booking.Booking
-	for rows.Next() {
-		var b booking.Booking
-		if err := rows.Scan(&b.ID, &b.User, &b.Desk, &b.Slot.Start, &b.Slot.End); err != nil {
-			return nil, fmt.Errorf("scan booking for date %q from database: %w", date, err)
-		}
-		ret = append(ret, b)
-	}
-	return ret, nil
-}
-
-func (store *Bookings) GetFutureBookingsForUser(ctx context.Context, user string) ([]booking.Booking, error) {
-	rows, err := store.db.QueryContext(ctx, querySelectFutureBookingsByUser, user)
-	switch {
-	case errors.Is(err, sql.ErrNoRows):
-	case err != nil:
-		return nil, fmt.Errorf("retrieve bookings for user %q from database: %w", user, err)
-	}
-	defer func() {
-		if cerr := rows.Close(); cerr != nil && err == nil {
-			err = cerr
-		}
-	}()
-	var ret []booking.Booking
-	for rows.Next() {
-		var b booking.Booking
-		if err := rows.Scan(&b.ID, &b.User, &b.Desk, &b.Slot.Start, &b.Slot.End); err != nil {
-			return nil, fmt.Errorf("scan booking for user %q from database: %w", user, err)
-		}
-		ret = append(ret, b)
-	}
-	return ret, nil
-}
-
 func (store *Bookings) DeleteBooking(ctx context.Context, id booking.ID) error {
-	if _, err := store.db.ExecContext(ctx, queryDeleteBookingByID, id); err != nil {
+	if _, err := store.db.ExecContext(ctx, execDeleteBooking, id); err != nil {
 		return fmt.Errorf("delete booking %d from database: %w", id, err)
 	}
 	return nil
+}
+
+func (store *Bookings) getDesks(ctx context.Context, query string, arg any) (_ []booking.Booking, err error) {
+	rows, err := store.db.QueryContext(ctx, query, arg)
+	switch {
+	case errors.Is(err, sql.ErrNoRows):
+	case err != nil:
+		return nil, fmt.Errorf("retrieve bookings from database: %w", err)
+	}
+	defer func() {
+		if closeErr := rows.Close(); closeErr != nil && err == nil {
+			err = closeErr
+		}
+	}()
+	var ret []booking.Booking
+	for rows.Next() {
+		var b booking.Booking
+		if err := rows.Scan(&b.ID, &b.User, &b.Desk, &b.Slot.Start, &b.Slot.End); err != nil {
+			return nil, fmt.Errorf("scan booking from database: %w", err)
+		}
+		ret = append(ret, b)
+	}
+	return ret, nil
+}
+
+func (store *Bookings) GetDeskBookings(ctx context.Context, desk string) ([]booking.Booking, error) {
+	return store.getDesks(ctx, queryBookingsByDesk, desk)
+}
+
+func (store *Bookings) GetAllBookingsForDate(ctx context.Context, date time.Time) ([]booking.Booking, error) {
+	return store.getDesks(ctx, queryBookingsByDate, date)
+}
+
+func (store *Bookings) GetFutureBookingsForUser(ctx context.Context, user string) ([]booking.Booking, error) {
+	return store.getDesks(ctx, queryFutureBookingsByUser, user)
 }
