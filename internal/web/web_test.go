04b2a86468fcaef71abeaf4172da495e28d18582
Alisdair MacLeod <git@alisdairmacleod.co.uk>
2024-10-28 08:38:34 +0000

    Add copyright notice to new file

--- internal/web/web_test.go
+++ internal/web/web_test.go
@@ -1,3 +1,20 @@
+// Copyright 2024 Alisdair MacLeod <copying@alisdairmacleod.co.uk>
+//
+// This file is part of deskd.
+//
+// deskd is free software: you can redistribute it and/or modify it under the
+// terms of the GNU Affero General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option) any
+// later version.
+//
+// deskd is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+// details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with deskd. If not, see <https://www.gnu.org/licenses/>.
+
 package web_test
 
 import (
